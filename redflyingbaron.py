d77a114db80dd64375095f65b7ef223c0e506081
Laurent Peuch <cortex@worlddomination.be>
2014-06-16 00:28:33 +0200

    [enh] add initializer to RedFlyingBaron

--- redflyingbaron.py
+++ redflyingbaron.py
@@ -1,2 +1,6 @@
+from redbaron import RedBaron
+
+
 class RedFlyingBaron(object):
-    pass
+    def __init__(self, files):
+        self.files = dict(zip(files, map(RedBaron, map(lambda x: open(x, "r").read(), files))))
