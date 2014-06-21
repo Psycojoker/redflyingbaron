becdc8e3ca43d91dfdb08a6e5d791828cf6a5fda
Laurent Peuch <cortex@worlddomination.be>
2014-06-21 07:28:29 +0200

    [mod] clean code

--- redflyingbaron.py
+++ redflyingbaron.py
@@ -2,31 +2,6 @@ from collections import OrderedDict
 from redbaron import RedBaron
 
 
-test_files = [
-"../baron/baron/baron.py",
-"../baron/baron/conftest.py",
-"../baron/baron/dumper.py",
-"../baron/baron/formatting_grouper.py",
-"../baron/baron/future.py",
-"../baron/baron/grammator.py",
-"../baron/baron/grammator_control_structures.py",
-"../baron/baron/grammator_data_structures.py",
-"../baron/baron/grammator_imports.py",
-"../baron/baron/grammator_operators.py",
-"../baron/baron/grammator_primitives.py",
-"../baron/baron/grouper.py",
-"../baron/baron/helpers.py",
-"../baron/baron/indentation_marker.py",
-"../baron/baron/inner_formatting_grouper.py",
-"../baron/baron/parser.py",
-"../baron/baron/path.py",
-"../baron/baron/render.py",
-"../baron/baron/spliter.py",
-"../baron/baron/token.py",
-"../baron/baron/tokenizer.py",
-]
-
-
 class RedFlyingBaron(OrderedDict):
     @classmethod
     def from_paths(class_, files, verbose=False):
@@ -60,10 +35,5 @@ class RedFlyingBaron(OrderedDict):
         return super(RedFlyingBaron, self).__getitem__(key)
 
 
-def main():
-    red = RedFlyingBaron.from_paths(test_files[:5], verbose=True)
-    from ipdb import set_trace; set_trace()
-
-
 if __name__ == '__main__':
-    main()
+    pass
