de801ce3bad1bda0c0c3f9cba2db733e95b0c863
Laurent Peuch <cortex@worlddomination.be>
2014-06-21 07:21:27 +0200

    [enh] test that from_paths accept the verbose keyword

--- test_redflyingbaron.py
+++ test_redflyingbaron.py
@@ -10,3 +10,7 @@ def test_from_path():
     red = RedFlyingBaron.from_paths(["redflyingbaron.py"])
     assert red.keys() == ["redflyingbaron.py"]
     assert red["redflyingbaron.py"].dumps() == RedBaron(open("redflyingbaron.py", "r").read()).dumps()
+
+
+def test_from_path_verbose():
+    RedFlyingBaron.from_paths(["redflyingbaron.py"], verbose=True)
