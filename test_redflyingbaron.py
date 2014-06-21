1f15d5833c406b7a60b0fa7d325a6f766031fe16
Laurent Peuch <cortex@worlddomination.be>
2014-06-21 07:27:06 +0200

    [enh] test getitem with filename without extension

--- test_redflyingbaron.py
+++ test_redflyingbaron.py
@@ -31,3 +31,9 @@ def test_getitem_by_filename():
     red = RedFlyingBaron.from_paths(["./redflyingbaron.py", "./test_redflyingbaron.py"])
     assert red["redflyingbaron.py"].dumps() == RedBaron(open("redflyingbaron.py", "r").read()).dumps()
     assert red["test_redflyingbaron.py"].dumps() == RedBaron(open("test_redflyingbaron.py", "r").read()).dumps()
+
+
+def test_getitem_by_filename_without_extension():
+    red = RedFlyingBaron.from_paths(["./redflyingbaron.py", "./test_redflyingbaron.py"])
+    assert red["redflyingbaron"].dumps() == RedBaron(open("redflyingbaron.py", "r").read()).dumps()
+    assert red["test_redflyingbaron"].dumps() == RedBaron(open("test_redflyingbaron.py", "r").read()).dumps()
