f7ef4e6c6515924bd4b64c2102f7d05dfdb80174
Laurent Peuch <cortex@worlddomination.be>
2014-06-21 07:20:45 +0200

    [enh] test from_paths

--- test_redflyingbaron.py
+++ test_redflyingbaron.py
@@ -1,5 +1,12 @@
+from redbaron import RedBaron
 from redflyingbaron import RedFlyingBaron
 
 
 def test_empty():
     RedFlyingBaron()
+
+
+def test_from_path():
+    red = RedFlyingBaron.from_paths(["redflyingbaron.py"])
+    assert red.keys() == ["redflyingbaron.py"]
+    assert red["redflyingbaron.py"].dumps() == RedBaron(open("redflyingbaron.py", "r").read()).dumps()
